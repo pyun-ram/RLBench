5a035db81267865ffad14eca4e9bac4c9008551c
pyun <pyun@connect.ust.hk>
2025-04-17 18:15:43 +0800

    [Fix]: fix put_item_in_drawer movable_objects

--- rlbench/tasks/put_item_in_drawer.py
+++ rlbench/tasks/put_item_in_drawer.py
@@ -20,7 +20,10 @@ class PutItemInDrawer(Task):
         self._waypoint1 = Dummy('waypoint2')
         self._item = Shape('item')
         self.register_graspable_objects([self._item])
-        self._movable_objects = self._joints + [self._item]
+        self._movable_objects = [
+            self.get_base().get_object(f'drawer_{itm}')
+            for itm in self._options
+        ] + [self._item]
 
     def init_episode(self, index) -> List[str]:
         option = self._options[index]

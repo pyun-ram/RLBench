8dcb2757c8121d7e650d8b88e25cf1fb8a501cb7
pyun <pyun@connect.ust.hk>
2025-06-08 15:33:10 +0800

    [Feature]: add scripts

--- scripts/20250607/generate_data.sh
+++ scripts/20250607/generate_data.sh
@@ -1,10 +1,14 @@
 SAVE_PATH=./data/
 
-for task in reach_single_moving_target_with_gravity
+for task in reach_single_moving_target_on_the_table_cpst \
+            reach_two_moving_targets_on_the_table \
+            reach_single_moving_target_with_gravity \
+            reach_two_moving_targets_with_gravity \
+            reach_single_bouncing_ball
 do
 python3 tools/dataset_generator.py \
     --tasks=$task \
-    --episodes_per_task=10 \
+    --episodes_per_task=1 \
     --save_path=$SAVE_PATH \
     --image_size=256,256\
     --renderer=opengl \
--- scripts/20250607/visualize_data.sh
+++ scripts/20250607/visualize_data.sh
@@ -1,6 +1,10 @@
 SAVE_PATH=./data/
 
-for task in reach_single_moving_target_with_gravity
+for task in  reach_single_moving_target_on_the_table_cpst \
+            reach_two_moving_targets_on_the_table \
+            reach_single_moving_target_with_gravity \
+            reach_two_moving_targets_with_gravity \
+            reach_single_bouncing_ball
 do
 python3 tools/dataset_visualizer.py visualize_episodes \
     --task=${task} \

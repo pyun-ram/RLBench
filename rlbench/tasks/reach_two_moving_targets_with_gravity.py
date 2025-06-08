478a73326e1abea18eae80abb4692cd874eb2d0e
pyun <pyun@connect.ust.hk>
2025-06-08 14:39:53 +0800

    [Feature]: refactorize reach_two_moving_targets_with_gravity

--- rlbench/tasks/reach_two_moving_targets_with_gravity.py
+++ rlbench/tasks/reach_two_moving_targets_with_gravity.py
@@ -1,10 +1,7 @@
-from typing import List, Tuple
-from rlbench.backend.task import Task
 from pyrep.objects import ProximitySensor, Shape, Dummy
-from rlbench.backend.conditions import DetectedCondition
 import numpy as np
-from tqdm import tqdm
-from pathlib import Path
+from .reach_single_moving_target_with_gravity import (
+    ReachSingleMovingTargetWithGravity, init_target_state)
 
 def get_state_config(var_index: int):
     if var_index == 0:
@@ -14,185 +11,37 @@ def get_state_config(var_index: int):
     else:
         raise ValueError("var_index must be 0, 1")
 
-def init_target_state(
-    area: List[float],
-    t_max: float,
-    x0: List[float] = None,
-    v0: List[float] = None,
-    a0: List[float] = None,
-    dt: float = 0.05,
-    direction: str = 'left',
-):
-    '''
-    Args:
-        area: List[float], [xmin,ymin,zmin,xmax,ymax,zmax] in Fworld
-        t_max: float, max time
-        x0: List[float], initial position, 
-        v0: float, initial velocity
-        a0: float, initial acceleration
-        dt: float
-    Return:
-        List[float], target state (x, v, a)
-    '''
-    assert direction in ['left', 'right']
-
-    dx = [0.05, 0.05, 0.05]
-    dv = [0.01, 0.01, 0.01]
-    da = [0.001, 0.001, 0.001]
-    if direction == 'left':
-        x_range = [area[0], area[1], area[5], area[3], area[1], area[5]] if x0 is None else [x0[0], x0[1], x0[2], x0[0], x0[1], x0[2]]
-        v_range = [0, 0, 0, 0, 0.1, 0.1] if v0 is None else [v0[0], v0[1], v0[2], v0[0], v0[1], v0[2]]
-    elif direction == 'right':
-        x_range = [area[0], area[4], area[5], area[3], area[4], area[5]] if x0 is None else [x0[0], x0[1], x0[2], x0[0], x0[1], x0[2]]
-        v_range = [0, -0.1, 0, 0, 0, 0.1] if v0 is None else [v0[0], v0[1], v0[2], v0[0], v0[1], v0[2]]
-    a_range = [0, 0, -0.035, 0, 0, 0] if a0 is None else [a0[0], a0[1], a0[2], a0[0], a0[1], a0[2]]
-
-    cache_name = "_".join([f'{itm}' for itm in x_range+v_range+a_range+dx+dv+da])
-    cache_name = f"/tmp/{cache_name}.npy"
-    if not Path(cache_name).exists():
-        xs = np.arange(x_range[0], x_range[3] + dx[0]/2, dx[0])
-        ys = np.arange(x_range[1], x_range[4] + dx[1]/2, dx[1])
-        zs = np.arange(x_range[2], x_range[5] + dx[2]/2, dx[2])
-        vxs = np.arange(v_range[0], v_range[3] + dv[0]/2, dv[0])
-        vys = np.arange(v_range[1], v_range[4] + dv[1]/2, dv[1])
-        vzs = np.arange(v_range[2], v_range[5] + dv[2]/2, dv[2])
-        axs = np.arange(a_range[0], a_range[3] + da[0]/2, da[0])
-        ays = np.arange(a_range[1], a_range[4] + da[1]/2, da[1])
-        azs = np.arange(a_range[2], a_range[5] + da[2]/2, da[2])
-
-        grid = np.array(np.meshgrid(xs, ys, zs, vxs, vys, vzs, axs, ays, azs, indexing='ij'))
-        points = grid.reshape(9, -1).T
-        t_samples = np.arange(0, t_max + dt/2, dt)
-
-        valid_mask = np.ones(points.shape[0], dtype=bool)
-        for t in tqdm(t_samples):
-            pos = points[:, 0:3] + points[:, 3:6] * t + 0.5 * points[:, 6:9] * t**2
-            in_box = (
-                (area[0] <= pos[:, 0]) & (pos[:, 0] <= area[3]) &
-                (area[1] <= pos[:, 1]) & (pos[:, 1] <= area[4]) &
-                (area[2] <= pos[:, 2]) & (pos[:, 2] <= area[5])
-            )
-            valid_velocity = np.linalg.norm(points[:, 3:6], axis=-1) >= 0.03
-            valid_acc = np.linalg.norm(points[:, 6:9], axis=-1) > 0.02
-            valid_mask &= in_box
-            valid_mask &= valid_velocity
-            valid_mask &= valid_acc
-            if not valid_mask.any():
-                break
-
-        valid_points = points[valid_mask]
-        Path(cache_name).parent.mkdir(parents=True, exist_ok=True)
-        np.save(cache_name, valid_points)
-    else:
-        valid_points = np.load(cache_name)
-    # sample one
-    idx = np.random.choice(valid_points.shape[0])
-    target_state = valid_points[idx]
-    target_state = target_state.tolist()
-    return target_state[:3], target_state[3:6], target_state[6:9]
-
-
-def compute_target_position(
-        t: float,
-        x0: List[float],
-        v0: List[float],
-        a0: List[float],
-        t0: float,
-        dt: float,
-    ) -> List[float]:
-    '''
-    Args:
-        t: float, current time
-        x0: List[float], initial position (x,y,z)
-        v0: List[float], initial velocity (vx, vy, vz)
-        a0: List[float], initial acceleration (ax, ay, az)
-        t0: float, initial time
-    Return:
-        List[float], target position (x,y,z)
-    '''
-    t_rel = t - t0
-    x0 = np.array(x0)
-    v0 = np.array(v0)
-    a0 = np.array(a0)
-    pos = x0 + v0 * t_rel + 0.5 * a0 * t_rel**2
-    return pos.tolist()
-
-def compute_delay(cur_position, tar_position):
-    '''
-    Args:
-        cur_position: List[float], current position (x,y,z)
-        tar_position: List[float], target position (x,y,z)
-    Return:
-        float, delay
-    '''
-    avr_speed = 0.2 # (m/s)
-    return np.linalg.norm(cur_position - tar_position) / avr_speed
-    
-
-class ReachTwoMovingTargetsWithGravity(Task):
-
-    def init_task(self) -> None:
-        self.success_sensor = ProximitySensor('success')
-        self.waypoint0 = Dummy('waypoint0')
-        self.target = Shape('target')
-        # [xmin,ymin,zmin,xmax,ymax,zmax] in Fworld
-        self.area = [0, -0.5, 0.8, 0.4, 0.5, 1.2]
-        self.t_max = 1 # (s)
-        self.step_id = 0
-        self.t = 0
-        self.target_state_list = []
-        self.condition = DetectedCondition(self.robot.arm.get_tip(), self.success_sensor)
-        self.register_success_conditions([self.condition])
-        self.register_waypoint_ability_start(0, self._move_above_object)
-        self.register_waypoints_should_repeat(self._repeat)
-        self.counter = 0
-        self.var_index = None
-        return
-
-    def init_episode(self, index: int) -> List[str]:
-        self.var_index = index
-        direction = get_state_config(self.var_index)
-        x, v, a = init_target_state(
-            self.area,
-            self.t_max,
-            x0=None,
-            v0=None,
-            a0=None,
-            direction=direction[self.counter]
-        )
-        # save target_state
-        self.target_state_list.append({
-            "x": x,
-            "v": v,
-            "a": a,
-            "t0": 0,
-        })
-        self.t = 0
-        self.target.set_position(x)
-        return [
-            "reach single moving target",
-            "reach single moving target on the table",
-        ]
-
-    def variation_count(self) -> int:
-        return 2
+class ReachTwoMovingTargetsWithGravity(ReachSingleMovingTargetWithGravity):
 
     def step(self) -> None:
-        self.step_id += 1
-        simulation_timestep = self.pyrep.get_simulation_timestep()
-        self.t += simulation_timestep
         if self.condition.condition_met()[0] and self.counter < 1:
             self.counter += 1
-            direction = get_state_config(self.var_index)
+            direction = get_state_config(self.var_index)[self.counter]
+            area = self.area
+            min_velo_norm = 0.03
+            min_acc_norm = 0.01
+            if direction == 'left':
+                x_range = [area[0], area[1], area[5], area[3], area[1], area[5]]
+                v_range = [0, 0, 0, 0, 0.1, 0.1]
+            elif direction == 'right':
+                x_range = [area[0], area[4], area[5], area[3], area[4], area[5]]
+                v_range = [0, -0.1, 0, 0, 0, 0.1]
             x = cur_x = self.target.get_position()
             while np.linalg.norm(x - cur_x) < 0.2:
                 x, v, a = init_target_state(
-                    self.area,
-                    self.t_max,
+                    t_max=self.t_max,
+                    area=area,
+                    x_range=x_range,
+                    v_range=v_range,
+                    a_range=[0, 0, -0.035, 0, 0, 0],
                     x0=None,
                     v0=None,
                     a0=None,
-                    direction=direction[self.counter]
+                    dx = [0.05, 0.05, 0.05],
+                    dv = [0.01, 0.01, 0.01],
+                    da = [0.001, 0.001, 0.001],
+                    min_velo_norm=min_velo_norm,
+                    min_acc_norm=min_acc_norm,
                 )
             self.target_state_list.append({
                 "x": x,
@@ -201,49 +50,14 @@ class ReachTwoMovingTargetsWithGravity(Task):
                 "t0": self.t,
             })
             self.target.set_position(x)
-        target_state_dict = self.target_state_list[-1]
-        target_position = compute_target_position(
-            t=self.t,
-            t0=target_state_dict["t0"],
-            x0=target_state_dict["x"],
-            v0=target_state_dict["v"],
-            a0=target_state_dict["a"],
-            dt=simulation_timestep,
-        )
-        self.target.set_position(target_position)
-        return
+        return super().step()
 
-    def cleanup(self) -> None:
-        # Called during at the end of each episode. Remove this if not using.
+    def init_task(self):
+        super().init_task()
         self.counter = 0
-
-    def _move_above_object(self, waypoint):
-        # compensate for grasping delay
-        tip_cur_position = self.robot.arm.get_tip().get_position()
-        tip_tar_position = self.target.get_position()
-        simulation_timestep = self.pyrep.get_simulation_timestep()
-        t_delay = compute_delay(
-            tip_cur_position,
-            tip_tar_position,
-        )
-        target_state_dict = self.target_state_list[-1]
-        new_wp_position = compute_target_position(
-            t = self.t+t_delay,
-            t0=self.t,
-            x0=tip_tar_position,
-            v0=target_state_dict["v"],
-            a0=target_state_dict["a"],
-            dt = simulation_timestep,
-        )
-        way_obj = waypoint.get_waypoint_object()
-        way_obj.set_position(new_wp_position)
         return
 
-    def _repeat(self):
-        return True
-
-    def base_rotation_bounds(self) -> Tuple[List[float], List[float]]:
-        return [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]
-
-    def is_static_workspace(self) -> bool:
-        return True
+    def cleanup(self):
+        super().cleanup()
+        self.counter = 0
+        return

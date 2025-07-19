eb693095775279c85ffe3b7a329a2c06993863ae
pyun <pyun@cse.ust.hk>
2025-07-19 15:41:38 +0000

    [Exp]: add scripts robot_data_20250719

--- scripts/20250719/generate_data_table.sh
+++ scripts/20250719/generate_data_table.sh
@@ -1,10 +1,12 @@
-SAVE_PATH=./data/dev/train_upcam/
+SAVE_PATH=./data/20250719_robot_data/train/
 
-for task in reach_single_moving_target_on_the_table_cpst
+for task in reach_single_moving_target_on_the_table_cpst \
+    reach_single_moving_target_with_gravity \
+    reach_single_bouncing_ball
 do
 xvfb-run -a python3 tools/dataset_generator.py \
     --tasks=$task \
-    --episodes_per_task=1 \
+    --episodes_per_task=70 \
     --save_path=$SAVE_PATH \
     --image_size=256,256\
     --renderer=opengl \

c2666cfd567f876e9150312d3bea2d3bac4c3e90
pyun <pyun@connect.ust.hk>
2025-11-06 10:19:05 +0800

    [Feature]: fix insert onto rotating peg step

--- rlbench/tasks/insert_onto_rotating_peg.py
+++ rlbench/tasks/insert_onto_rotating_peg.py
@@ -254,9 +254,9 @@ class InsertOntoRotatingPeg(Task):
             if self.step_id % 10 == 0:
                 self._path, self._open = self.expert_plan()
                 self._path_done = False
-            if not self._path_done:
+            if self._path is not None and not self._path_done:
                 self._path_done = self._path.step()
-            if self._path_done:
+            if (self.step_id + 1) % 10 == 0:
                 self.move_gripper_tip([self._open])
 
         self.step_id += 1

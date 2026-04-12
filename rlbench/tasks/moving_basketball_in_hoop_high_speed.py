80880a2837ef63756ec16ec7631eec2e0f7e840e
pyun <pyun@connect.ust.hk>
2026-04-12 09:38:43 +0800

    [Feature]: add boundary at moving basketball high speed task

--- rlbench/tasks/moving_basketball_in_hoop_high_speed.py
+++ rlbench/tasks/moving_basketball_in_hoop_high_speed.py
@@ -207,7 +207,7 @@ class MovingBasketballInHoopHighSpeed(Task):
             self.ball_area[5],
         ]
         target_size_xy = [0.15, 0.05]
-        self.hoop_area = [0.4, -0.5, 0.8, 0.6, 0.5, 0.8]
+        self.hoop_area = [0.3, -0.5, 0.8, 0.6, 0.5, 0.8]
         self.hoop_area = [
             self.hoop_area[0]+target_size_xy[0]/2,
             self.hoop_area[1]+target_size_xy[1]/2,
